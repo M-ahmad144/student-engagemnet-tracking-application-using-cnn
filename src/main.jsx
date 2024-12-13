7be9cf22e6366a0e169c992b44ce1f3349d74c4b
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2024-12-13 18:51:03 +0500

    Error in deployment

--- src/main.jsx
+++ src/main.jsx
@@ -13,8 +13,8 @@ import "./index.css";
 import store from "./redux/store";
 
 // Auth Pages
-import Login from "./pages/Auth/Login.jsx";
-import Register from "./pages/Auth/Register.jsx";
+import Login from "./Pages/Auth/Login.jsx";
+import Register from "./Pages/Auth/Register.jsx";
 
 // User Pages
 import Profile from "./Pages/User/Profile.jsx";
@@ -39,7 +39,7 @@ const theme = createTheme({
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="/" element={<HomePage />} />
+      <Route path="" element={<HomePage />} />
       <Route path="/analysis-result" element={<Analysis />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
