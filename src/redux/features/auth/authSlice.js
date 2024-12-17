e567b97308864b86e7f040e975a7375173918dbb
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2024-12-17 14:38:24 +0500

    fix cookies issue

--- src/redux/features/auth/authSlice.js
+++ src/redux/features/auth/authSlice.js
@@ -1,7 +1,8 @@
+import Cookies from "js-cookie";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  userInfo: null,
+  userInfo: JSON.parse(localStorage.getItem("userInfo")) || null,
 };
 
 const authSlice = createSlice({
@@ -10,18 +11,29 @@ const authSlice = createSlice({
   reducers: {
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
-
-      // Store user information in localStorage
-      localStorage.setItem("userInfo", JSON.stringify(action.payload));
     },
     logout: (state) => {
       state.userInfo = null;
-
-      // Clear user information from localStorage
-      localStorage.removeItem("userInfo");
     },
   },
 });
 
 export const { setCredentials, logout } = authSlice.actions;
+
+export const authMiddleware = (storeAPI) => (next) => (action) => {
+  if (action.type === setCredentials.type) {
+    const userInfo = action.payload;
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    Cookies.set("token", userInfo.token, {
+      secure: true,
+      sameSite: "strict",
+      expires: 1,
+    }); // 1 day
+  } else if (action.type === logout.type) {
+    localStorage.removeItem("userInfo");
+    Cookies.remove("token");
+  }
+  return next(action);
+};
+
 export default authSlice.reducer;
